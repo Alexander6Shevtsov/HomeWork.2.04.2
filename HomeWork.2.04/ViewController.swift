eec95cf449c12edfe98ed52acffa77c35d9f62c1
Alexander6Shevtsov <axrsvr@gmail.com>
2024-03-20 09:51:23 +0400

    Initial Commit

---
+++ HomeWork.2.04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork.2.04
+//
+//  Created by Alexander Shevtsov on 20.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
